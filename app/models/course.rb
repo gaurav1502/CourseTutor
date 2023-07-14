eb410bd3f082b692e1e90fcfc3a710a94a5a907a
gauravmedhane <gauravmedhane44@gmail.com>
2023-07-14 09:26:50 +0530

    Added Course and Tutor model with accepts_nested_attributes_for and has_many releationship

---
+++ app/models/course.rb
@@ -0,0 +1,8 @@
+class Course < ApplicationRecord
+  has_many :tutors, dependent: :destroy
+
+  validates :code, presence: true, uniqueness: true
+  validates :name, presence: true
+
+  accepts_nested_attributes_for :tutors
+end
---
+++ app/models/tutor.rb
@@ -0,0 +1,6 @@
+class Tutor < ApplicationRecord
+  belongs_to :course
+
+  validates :name, presence: true
+  validates :contact_number, presence: true, uniqueness: true, length: { is: 10 }
+end
