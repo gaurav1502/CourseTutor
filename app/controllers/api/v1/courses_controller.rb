683e33d611bc1bebf0eb1d079d83636070482b57
gauravmedhane <gauravmedhane44@gmail.com>
2023-07-14 09:23:40 +0530

    Added course controller with index and create access

---
+++ app/controllers/api/v1/courses_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::CoursesController < ApplicationController
+  # if params not present then we also handled this error in application ActionController::ParameterMissing
+  def index
+     # we can also use pagination here
+    course_data = Course.includes(:tutors).as_json(
+      only: %i[name code], include: { tutors: { only: %i[name contact_number] } }
+    )
+    render json: { data: course_data }
+  end
+
+  def create
+
+    @course = Course.new(course_params)
+    if @course.save
+      render json: { status: true, message: I18n.t('course.created') }, status: :ok
+    else
+      errors = @course.errors.full_messages.join(',')
+      # if error then response will status - false and message
+      render json: { status: false, message: errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  # Strong params for course
+  def course_params
+    params.require(:course)
+          .permit(:name, :code,
+            tutors_attributes: %i[name contact_number]
+                )
+  end
+end
